96ba37de16b11d5a84a9330e8ae0d58a612c1493
SHAMNAS KOYANI <shamnaskoyani@gmail.com>
2023-05-10 15:44:00 +0530

    Initial Commit

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Staging Banner',
+    'description': 'Adds a banner to the top of the screen in staging instances',
+    'version': '1.0',
+    'author': 'Shamnas Koyani',
+    'depends': ['web'],
+    'data': ['views.xml'],
+    'application': False,
+}
